9fbb76e0e411e70f073de0e991b8ec1ab839540a
hakkinrou <hakkinrou@gmail.com>
2024-06-13 02:03:35 +0200

    Create index.meta.js

---
+++ index.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         hitomi.la Utilities
+// @author       hakkinrou
+// @version      1.0
+// @description  Utils addon for hitomi.la, adding features like notes, votes and whatever
+// @match        https://hitomi.la/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hitomi.la
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
