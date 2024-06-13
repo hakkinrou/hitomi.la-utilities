71f4b507c978e54b4cb16f0e8fab71b667319cc0
hakkinrou <hakkinrou@gmail.com>
2024-06-13 02:10:15 +0200

    Added downloadURL and updateURL meta

--- index.user.js
+++ index.user.js
@@ -5,6 +5,8 @@
 // @description  Utils addon for hitomi.la, adding features like notes, votes and whatever
 // @match        https://hitomi.la/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hitomi.la
+// @downloadURL  https://github.com/hakkinrou/hitomi.la-utilities/raw/main/index.user.js
+// @updateURL    https://github.com/hakkinrou/hitomi.la-utilities/raw/main/index.meta.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // ==/UserScript==
